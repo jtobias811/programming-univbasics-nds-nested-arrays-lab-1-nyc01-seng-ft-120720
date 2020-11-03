9c7f3749173dad5bf3be47f014183c05879e80f3
Jacob Tobias <jtobias811@gmail.com>
2020-11-03 17:58:36 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,7 +17,7 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  assembled_matrix = [ORGANIC_PRODUCE, CONVENTIONAL_PRODUCE]
+  assembled_matrix = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
 end
 
 def sorted_matrix
