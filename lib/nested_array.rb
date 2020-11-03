094ad464fdd2498e3b73ff8b30d5f3450c6952e5
Jacob Tobias <jtobias811@gmail.com>
2020-11-03 19:01:05 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -29,5 +29,5 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  assembled_matrix[0][2] = "Lemons"
+  assembled_matrix[row][column] = "new_value"
 end
